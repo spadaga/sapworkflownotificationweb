d347fc7ee23a58d96552ff0d1efc71e1bfa88f97
gpadaga <padagatools@gmail.com>
2025-06-25 22:20:42 +0530

    bg color -heade

---
+++ server.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const port = process.env.PORT || 8080;
+
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, Azure!\n');
+}).listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
